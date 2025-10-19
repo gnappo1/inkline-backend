4ee887c4c5a280e9e522b2d8340e6e5463270f59
Matteo Piccini <gnappo.1@live.it>
2025-10-19 00:37:30 -0700

    cookie issue persists

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 Rails.application.config.session_store :cookie_store,
   key: '_inkline_api_session',
-  domain: (ENV['SESSION_COOKIE_DOMAIN'].presence || :all),                                    # <- works for subdomains
+  domain: (ENV['SESSION_COOKIE_DOMAIN'].presence || :nil),                                    # <- works for subdomains
   tld_length: 2,                                   # "inkline.live"
-  same_site: :lax,                                 # now same-site with subdomains
+  same_site: :none,                                 # now same-site with subdomains
   secure: Rails.env.production?

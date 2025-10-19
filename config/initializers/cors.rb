877cef1e9cd627d66dc71a854fa07adffc5d1948
Matteo Piccini <gnappo.1@live.it>
2025-10-18 23:50:01 -0700

    update default origin for CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV.fetch("FRONTEND_ORIGIN", "http://localhost:5173")
+    origins ENV.fetch("FRONTEND_ORIGIN", "https://app.inkline.live")
     resource "*", headers: :any, methods: %i[get post patch put delete options head], credentials: true
   end
 end
